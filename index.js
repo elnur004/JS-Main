fd7093c6528f350f9ce79396cc774925be1aaf3c
elnur004 <lnur004@gmail.com>
2023-05-08 15:44:27 +0400

    added separate functions for user logic and for message

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const RESULT_DRAW = 'DRAW';
+const DEFAULT_CHOICE = ROCK;
 
 // Generated random values for computer
 const computerPlay = () => {
@@ -14,6 +15,24 @@ const computerPlay = () => {
   } else {
     return SCISSORS;
   }
+};
+
+const userPlay = () => {
+  // Choise the value via prompt window
+  const userInput = prompt(`Enter either ${ROCK}, ${PAPER} or ${SCISSORS}`)
+    .trim()
+    .toUpperCase();
+
+  // Check the user enters a valid input
+  if (userInput !== ROCK && userInput !== PAPER && userInput !== SCISSORS) {
+    alert(
+      `${
+        userInput === '' ? 'Empty value' : userInput
+      } is INVALID CHOICE! ${DEFAULT_CHOICE} has been choicen for you!`
+    );
+    return DEFAULT_CHOICE;
+  }
+  return userInput;
 };
 
 // Play a single round
@@ -31,53 +50,16 @@ const playRound = (playerSelection, computerSelection) => {
   }
 };
 
-const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
+const message = (gameRound, userSelection, computerSelection) => {
+  // Show round and players choice
+  console.log(`Round: ${gameRound}`);
+  console.log(`Your choice is ${userSelection}`);
+  console.log(`Computer choice is ${computerSelection}`);
+  console.log(`${playRound(userSelection, computerSelection)}`);
+  console.log('----------------------');
+};
 
-  // Created 5 rounds play with for loop
-  for (let round = 1; round <= 5; round++) {
-    // Choise the value via prompt window
-    const userInput = prompt(
-      'Enter one of "rock", "paper" or "scissors"'
-    ).toUpperCase();
-
-    // Check the user enters a valid input
-    if (userInput !== ROCK && userInput !== PAPER && userInput !== SCISSORS) {
-      alert(
-        `${userInput} is INVALID INPUT! Enter eather ${ROCK}, ${PAPER} or ${SCISSORS}`
-      );
-    }
-
-    // Generate computer choise value via computerPlay() function
-    const computerChoise = computerPlay();
-
-    // Call playRound() function and pass proper arguments for a single round
-    const singleRound = playRound(userInput, computerChoise);
-
-    // Play the single round and update score
-    if (
-      singleRound.includes(
-        `You win the round! ${userInput} beats ${computerChoise}`
-      )
-    ) {
-      playerScore++;
-    } else if (
-      singleRound.includes(
-        `You Lose the round! ${computerChoise} beats ${userInput}`
-      )
-    ) {
-      computerScore++;
-    }
-
-    // Show round and players choice
-    console.log(`Round: ${round}`);
-    console.log(`Your choice is ${userInput}`);
-    console.log(`Computer choice is ${computerChoise}`);
-    console.log(`${playRound(userInput, computerChoise)}`);
-    console.log('----------------------');
-  }
-
+const winnerGame = (playerScore, computerScore) => {
   // Compare the scores and determin winner of the game
   if (playerScore > computerScore) {
     console.log(
@@ -85,12 +67,46 @@ const game = () => {
     );
   } else if (computerScore > playerScore) {
     console.log(
-      `You lose the game! The score is ${playerScore} - ${computerScore}`
+      `You lose the game! The score is ${computerScore} - ${playerScore}`
     );
   } else {
     console.log(
-      `The game ends in a ${RESULT_DRAW}! The score is ${+playerScore}-${+computerScore}.`
+      `The game ends in a ${RESULT_DRAW}! The score is ${playerScore}-${computerScore}.`
     );
   }
+};
+
+const game = () => {
+  let userScore = 0;
+  let compScore = 0;
+
+  // Created 5 rounds play with for loop
+  for (let round = 1; round <= 5; round++) {
+    // Generate computer choise value via computerPlay() function
+    const computerChoise = computerPlay();
+
+    const userChoice = userPlay();
+
+    // Call playRound() function and pass proper arguments for a single round
+    const singleRound = playRound(userChoice, computerChoise);
+
+    // Play the single round and update score
+    if (
+      singleRound.includes(
+        `You win the round! ${userChoice} beats ${computerChoise}`
+      )
+    ) {
+      userScore++;
+    } else if (
+      singleRound.includes(
+        `You Lose the round! ${computerChoise} beats ${userChoice}`
+      )
+    ) {
+      compScore++;
+    }
+
+    message(round, userChoice, computerChoise);
+  }
+  winnerGame(userScore, compScore);
 };
 game();
